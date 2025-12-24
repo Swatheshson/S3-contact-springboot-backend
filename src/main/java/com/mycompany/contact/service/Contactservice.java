1047a1ecc1b75cf311690e23fa08e8cd40c4ef3a
unknown <swathesh.ags@gmail.com>
2025-12-24 23:24:44 +0530

    working in postconstruct part in service file

--- src/main/java/com/mycompany/contact/service/Contactservice.java
+++ src/main/java/com/mycompany/contact/service/Contactservice.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
+
 import org.springframework.stereotype.Service;
 
 import com.google.gson.Gson;
@@ -20,7 +21,9 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import java.lang.reflect.Type;
 
 @Service
-public class Contactservice {
+public class ContactService {
+	
+	
 
     private final String BUCKET_NAME = "my-contact-bucket-swatheshson";
     private final String FILE_KEY = "contacts.json";
@@ -32,6 +35,7 @@ public class Contactservice {
     @PostConstruct
     public void init() {
         // Initialize S3 client
+    	System.out.println("task startded");
         s3 = S3Client.builder().region(Region.US_EAST_1).build();
 
         // Load existing contacts from S3
@@ -41,44 +45,16 @@ public class Contactservice {
 
             Type type = new TypeToken<Map<String, String>>() {}.getType();
             contacts = gson.fromJson(json, type);
-
+            System.out.println("task endedd");
             System.out.println("Contacts loaded from S3: " + contacts);
+            
         } catch (Exception e) {
             System.out.println("⚠ No existing contacts found. Starting fresh.");
             contacts = new HashMap<>();
         }
     }
 
-    // Add or update a contact
-    public void addOrUpdateContact(String name, String phone) {
-        contacts.put(name, phone);  // Update in-memory map
-        saveToS3();                 // Persist updated map to S3
-    }
-
-    // Optional: delete a contact
-    public void deleteContact(String name) {
-        contacts.remove(name);
-        saveToS3();
-    }
-
-    // Save current map to S3
-    private void saveToS3() {
-        String jsonContent = gson.toJson(contacts);
-
-        PutObjectRequest request = PutObjectRequest.builder()
-                .bucket(BUCKET_NAME)
-                .key(FILE_KEY)
-                .serverSideEncryption("AES256")
-                .contentType("application/json")
-                .build();
-
-        s3.putObject(request, RequestBody.fromBytes(jsonContent.getBytes(StandardCharsets.UTF_8)));
-
-        System.out.println("✅ Contacts uploaded to S3 successfully!");
-    }
-
-    public Map<String, String> getContacts() {
-        return contacts;
-    }
+   
+    
 }
 
