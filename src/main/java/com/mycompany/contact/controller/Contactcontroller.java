ddb7854b377a3ff673ba20b9afaaf71abff5fc82
unknown <swathesh.ags@gmail.com>
2025-12-28 21:43:35 +0530

    front - backend save contact method

--- src/main/java/com/mycompany/contact/controller/Contactcontroller.java
+++ src/main/java/com/mycompany/contact/controller/Contactcontroller.java
@@ -1,19 +1,30 @@
 package com.mycompany.contact.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.mycompany.contact.dto.Contact;
+import com.mycompany.contact.service.ContactService;
 
 @RestController  
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/addcontacts") // Base path
-public class Contactcontroller {
+
+
+
+
+public class Contactcontroller extends ContactService {
+	
+	@Autowired
+	private ContactService contactservice;
 
     // POST endpoint to receive data
     // Handles POST requests to: /addcontacts
     @PostMapping
     public String receiveContact(@RequestBody Contact contact) {
-        System.out.println("Received: " + contact.getName());
-        System.out.println("Received: " + contact.getPhone());
+       //received contact should be saved in the hashmap object
+    	
+    	savetohashmapobj(contact.getName(),contact.getPhone());
+    	
         return "Contact received successfully!";
     }
 }
