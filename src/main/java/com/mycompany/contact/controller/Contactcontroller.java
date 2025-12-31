19c6d4238fc86e18662201c64647c379987100a5
unknown <swathesh.ags@gmail.com>
2025-12-31 20:58:10 +0530

    making changes to endpoints and clearing it

--- src/main/java/com/mycompany/contact/controller/Contactcontroller.java
+++ src/main/java/com/mycompany/contact/controller/Contactcontroller.java
@@ -9,26 +9,26 @@ import com.mycompany.contact.service.ContactService;
 
 @RestController  
 @CrossOrigin(origins = "http://localhost:4200")
-public class Contactcontroller extends ContactService {
+public class Contactcontroller {
 	
 	@Autowired
 	private ContactService contactservice;
 
     // POST endpoint to receive data
     // Handles POST requests to: /addcontacts
-    @PostMapping
+    @PostMapping("/add")
     public String receiveContact(@RequestBody Contact contact) {
        //received contact should be saved in the hashmap object
     	
-    	savetohashmapobj(contact.getName(),contact.getPhone());
+    	contactservice.savetohashmapobj(contact.getName(),contact.getPhone());
     	
         return "Contact received successfully!";
     }
     
     
-   @GetMapping
+   @GetMapping("/show")
    public  Map<String,String> sendcontactstofront(){
     	
-    	return contactservice.
+    	return contactservice.sendcontactstofront();
     }
 }
